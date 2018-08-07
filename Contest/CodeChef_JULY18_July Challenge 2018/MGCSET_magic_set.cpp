7e57fa92a2b16061b3072c26cfd67a4e2ccfc253
Ghundo <marcavenzaid.gamil@gmail.com>
2018-08-08 01:14:16 +0800

    Removed unnecessary comments

--- Contest/CodeChef_JULY18_July Challenge 2018/MGCSET_magic_set.cpp
+++ Contest/CodeChef_JULY18_July Challenge 2018/MGCSET_magic_set.cpp
@@ -1,30 +1,3 @@
-/* Problem:
- * Katya has a sequence of integers a1,a2,…,an and an integer m.
- *
- * She defines a good sequence of integers as a non-empty sequence such that the
- * sum of the elements in each of its non-empty subsequences is divisible by m.
- *
- * Katya wants to know the number of good subsequences of the sequence a.
- * Can you help her?
- *
- * Input:
- * • The first line of the input contains a single integer T denoting the number
- *   of test cases. The description of T test cases follows.
- * • The first line of each test case contains two space-separated integers n
- *   and m.
- * • The second line contains n space-separated integers a1,a2,…,an.
- *
- * Output:
- * For each test case, print a single line containing one integer - the number
- * of good subsequences.
- *
- * Constraints:
- * • 1 <= T <= 1,000
- * • 1 <= n <= 30
- * • 1 <= m <= 1,000
- * • 1 <= ai <= 1,000 for each valid i
- */
-
 /**
  * author: marcavenzaid
  * created: 2018-07-08-02.48
